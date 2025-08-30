b159b23b4d30bdc91e62407cfd9f62b778e036c5
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2025-08-30 15:31:30 -0300

    feat(api): add register user usecase

---
+++ apps/api/src/modules/auth/application/use-cases/register-user.usecase.ts
@@ -0,0 +1,38 @@
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import * as UserRepository from '../../domain/ports/user-repository.port';
+import * as PasswordHasher from '../../domain/ports/password-hasher.port';
+
+export type RegisterUserInput = {
+  name: string;
+  email: string;
+  password: string;
+};
+export type RegisterUserOutput = { id: string; name: string; email: string };
+
+@Injectable()
+export class RegisterUserUseCase {
+  constructor(
+    @Inject(UserRepository.USERS_REPOSITORY)
+    private readonly userRepository: UserRepository.UserRepositoryPort,
+    @Inject(PasswordHasher.PASSWORD_HASHER)
+    private readonly passwordHasher: PasswordHasher.PasswordHasherPort,
+  ) {}
+
+  async execute({
+    email,
+    name,
+    password,
+  }: RegisterUserInput): Promise<RegisterUserOutput> {
+    const exists = await this.userRepository.findByEmail(email);
+    if (!exists) {
+      throw new UnauthorizedException('Email already in use');
+    }
+
+    const passwordHash = await this.passwordHasher.hash(password);
+    return this.userRepository.create({
+      email,
+      name,
+      passwordHash,
+    });
+  }
+}
